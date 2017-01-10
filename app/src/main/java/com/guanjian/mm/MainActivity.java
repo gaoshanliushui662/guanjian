93e51a9e5fa2df6c047d60e2d8b5a1b4916e03b3
赵耀升 <zhaoyshf@yonyou.com>
2017-01-10 23:42:49 +0800

    webview集成和拍照从相册选择

---
+++ app/src/main/java/com/guanjian/mm/MainActivity.java
@@ -0,0 +1,514 @@
+package com.guanjian.mm;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.ClipData;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcelable;
+import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+
+    private WebView webview;
+    private Button btn1;
+    private Button btn2;
+    private ImageView iv_photo;
+    private PopupWindow mPopupWindow;
+    public final static int ALBUM_REQUEST_CODE = 1;
+    public final static int CROP_REQUEST = 2;
+    public final static int CAMERA_REQUEST_CODE = 3;
+    // 拍照路径
+    public static String SAVED_IMAGE_DIR_PATH =
+            Environment.getExternalStorageDirectory().getPath()
+                    + "/AppName/camera/";
+    private String mCurrentPhotoPath;
+    private static final int REQ_TAKE_PHOTO = 444;
+    private static final int FROM_ALBUM = 555;
+    private static final int CROP_CODE = 666;
+    public static final int REQUEST_SELECT_FILE = 100;
+    public final static int FILECHOOSER_RESULTCODE = 1;
+    public ValueCallback<Uri[]> uploadMessage;
+    public ValueCallback<Uri> mUploadMessage;
+//    public ProgressBar mWebLoadingProgressBar;
+    private MainActivity activity;
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        activity = this;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 200);
+        }
+        final IWXAPI msgApi = WXAPIFactory.createWXAPI(this, null);
+        msgApi.registerApp("wxdb8bcc7ce0a3b51c");
+        webview = (WebView) findViewById(R.id.webView1);
+//        webview.loadUrl("https://app.cv-china.com");
+        webview.loadUrl("http://www.zhonghaonan.com/");
+        WebSettings settings = webview.getSettings();
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setJavaScriptEnabled(true);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        iv_photo = (ImageView) findViewById(R.id.iv_photo);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 无参数调用
+                webview.loadUrl("javascript:javacalljs()");
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 传递参数调用
+                webview.loadUrl("javascript:javacalljswith(" + "'zhaoyaosheng'" + ")");
+            }
+        });
+
+        webview.addJavascriptInterface(MainActivity.this, "android");
+        webview.setWebChromeClient(new MyWebClient());
+        webview.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+
+    public class MyWebClient extends WebChromeClient {
+        //For Android 4.1
+        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+//            openFileChooser(uploadMsg);
+            mUploadMessage = uploadMsg;
+            showPopupWindow();
+//            initiateRequest();
+/*            activity.mUploadMessage = uploadMsg;
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("image*//*");
+            activity.startActivityForResult(Intent.createChooser(i, "File Chooser"), activity.FILECHOOSER_RESULTCODE);*/
+
+        }
+
+        //For Android 5.0
+        @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+        public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+            // make sure there is no existing message
+            uploadMessage = filePathCallback;
+            showPopupWindow();
+//            initiateRequest();
+/*            if (activity.uploadMessage != null) {
+                activity.uploadMessage.onReceiveValue(null);
+                activity.uploadMessage = null;
+            }
+            activity.uploadMessage = filePathCallback;
+            Intent intent = fileChooserParams.createIntent();
+            try {
+                activity.startActivityForResult(intent, activity.REQUEST_SELECT_FILE);
+            } catch (ActivityNotFoundException e) {
+                activity.uploadMessage = null;
+                return false;
+            }*/
+            return true;
+        }
+
+        private void initiateRequest() {
+            Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+            i.addCategory(Intent.CATEGORY_OPENABLE);
+            i.setType("image/*");
+            startActivityForResult(Intent.createChooser(i,"上传文件"),FILECHOOSER_RESULTCODE);
+        }
+
+    }
+
+    /**
+     * 调用微信支付接口
+     * @param params
+     */
+    @JavascriptInterface
+    public void callWeinXinPay(String params){
+
+    }
+
+    @JavascriptInterface
+    public void startFunction(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "你好", Toast.LENGTH_LONG).show();
+                showPopupWindow();
+            }
+        });
+//        showPopupWindow();
+    }
+
+    private void showPopupWindow() {
+        View popupView = getLayoutInflater().inflate(R.layout.camera, null);
+        mPopupWindow = new PopupWindow(popupView, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, true);
+        mPopupWindow.setTouchable(true);
+        mPopupWindow.setOutsideTouchable(true);
+//        mPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
+        TextView tv_camera = (TextView) popupView.findViewById(R.id.tv_camera);
+        TextView tv_album = (TextView) popupView.findViewById(R.id.tv_album);
+        TextView tv_cancel = (TextView) popupView.findViewById(R.id.tv_cancel);
+
+        tv_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+            }
+        });
+        tv_album.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAlbum();
+            }
+        });
+        tv_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(uploadMessage != null){
+                    Uri[] uris = new Uri[1];
+                    uris[0] = Uri.parse("");
+                    uploadMessage.onReceiveValue(uris);
+                    uploadMessage=null;
+                }else {
+                    mUploadMessage.onReceiveValue(Uri.parse(""));
+                    mUploadMessage=null;
+                }
+                if (mPopupWindow != null && mPopupWindow.isShowing()) {
+                    mPopupWindow.dismiss();
+                }
+            }
+        });
+        if (mPopupWindow != null && !mPopupWindow.isShowing()) {
+            mPopupWindow.showAtLocation(webview, Gravity.BOTTOM, 0, 0);
+        }
+    }
+
+    private void openAlbum() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, FROM_ALBUM);
+        if (mPopupWindow != null && mPopupWindow.isShowing()) {
+            mPopupWindow.dismiss();
+        }
+    }
+
+    private Uri uri = null;
+
+    private void openCamera() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {//判断是否有相机应用
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();//创建临时图片文件
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                //FileProvider 是一个特殊的 ContentProvider 的子类，
+                //它使用 content:// Uri 代替了 file:/// Uri. ，更便利而且安全的为另一个app分享文件
+                Uri uri = FileProvider.getUriForFile(MainActivity.this,
+                        "com.guanjian.mm",
+                        photoFile);
+                Log.i("zys", "photoURI:" + uri.toString());
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+                startActivityForResult(takePictureIntent, REQ_TAKE_PHOTO);
+                if (mPopupWindow != null && mPopupWindow.isShowing()) {
+                    mPopupWindow.dismiss();
+                }
+            }
+        }
+    }
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.CHINA).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        //.getExternalFilesDir()方法可以获取到 SDCard/Android/data/你的应用的包名/files/ 目录，一般放一些长时间保存的数据
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        //创建临时文件,文件前缀不能少于三个字符,后缀如果为空默认未".tmp"
+        File image = File.createTempFile(
+                imageFileName,  /* 前缀 */
+                ".jpg",         /* 后缀 */
+                storageDir      /* 文件夹 */
+        );
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @JavascriptInterface
+    public void startFunction(final String a){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, a, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (null == mUploadMessage || uploadMessage == null)
+            return;
+        switch (requestCode) {
+            case REQ_TAKE_PHOTO://返回结果
+                if (resultCode != Activity.RESULT_OK) return;
+                // Get the dimensions of the View
+                int targetW = iv_photo.getWidth();
+                int targetH = iv_photo.getHeight();
+
+                // Get the dimensions of the bitmap
+                BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+                bmOptions.inJustDecodeBounds = true;
+                BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+                int photoW = bmOptions.outWidth;
+                int photoH = bmOptions.outHeight;
+
+                // Determine how much to scale down the image
+                int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+                // Decode the image file into a Bitmap sized to fill the View
+                bmOptions.inJustDecodeBounds = false;
+                bmOptions.inSampleSize = scaleFactor;
+                bmOptions.inPurgeable = true;
+
+                Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+                iv_photo.setImageBitmap(bitmap);
+                break;
+            case FROM_ALBUM:
+
+                if (data == null){
+                    return;
+                }else{
+                    //用户从图库选择图片后会返回所选图片的Uri
+                    //获取到用户所选图片的Uri
+                    uri = data.getData();
+                    //返回的Uri为content类型的Uri,不能进行复制等操作,需要转换为文件Uri
+                    uri = convertUri(uri);
+                    startImageZoom(uri);
+                }
+                break;
+            case CROP_CODE:
+                if (data == null){
+                    return;
+                }else{
+                    Bundle extras = data.getExtras();
+                    if (extras != null){
+                        //获取到裁剪后的图像
+                        Bitmap bm = extras.getParcelable("data");
+                        iv_photo.setImageBitmap(bm);
+                    }
+                }
+                break;
+            case FILECHOOSER_RESULTCODE:
+                if (null == mUploadMessage && null == uploadMessage)
+                    return;
+                Uri result = data == null || resultCode != RESULT_OK ? null : data.getData();
+                if (uploadMessage != null){
+                    onActivityResultAboveL(requestCode, resultCode, data);
+                }else if (mUploadMessage != null){
+                    mUploadMessage.onReceiveValue(result);
+                    mUploadMessage = null;
+                }
+                break;
+            case REQUEST_SELECT_FILE:
+                if (uploadMessage == null) return;
+                uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data));
+                uploadMessage = null;
+                break;
+            default:
+                break;
+        }
+        if(uploadMessage != null){
+            Uri[] uris = new Uri[1];
+            uris[0] = uri;
+            uploadMessage.onReceiveValue(uris);
+        }else {
+            mUploadMessage.onReceiveValue(uri);
+        }
+        uploadMessage = null;
+        mUploadMessage = null;
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private void onActivityResultAboveL(int requestCode, int resultCode, Intent data){
+        if (requestCode != FILECHOOSER_RESULTCODE || uploadMessage == null){
+            return;
+        }
+        Uri[] results = null;
+        if (resultCode == Activity.RESULT_OK){
+            if (data == null){
+
+            }else{
+                String dataString = data.getDataString();
+                ClipData clipData = data.getClipData();
+                if (clipData != null){
+                    results = new Uri[clipData.getItemCount()];
+                    for (int i = 0; i < clipData.getItemCount(); i++){
+                        ClipData.Item item = clipData.getItemAt(i);
+                        results[i] = item.getUri();
+                    }
+                }
+                if (dataString != null){
+                    results = new Uri[]{
+                            Uri.parse(dataString)
+                    };
+                }
+            }
+        }
+        uploadMessage.onReceiveValue(results);
+        uploadMessage = null;
+    }
+
+    /**
+     * 将content类型的Uri转化为文件类型的Uri
+     * @param uri
+     * @return
+     */
+    private Uri convertUri(Uri uri){
+        InputStream is;
+        try {
+            //Uri ----> InputStream
+            is = getContentResolver().openInputStream(uri);
+            //InputStream ----> Bitmap
+            Bitmap bm = BitmapFactory.decodeStream(is);
+            //关闭流
+            is.close();
+            return saveBitmap(bm, "temp");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 将Bitmap写入SD卡中的一个文件中,并返回写入文件的Uri
+     * @param bm
+     * @param dirPath
+     * @return
+     */
+    private Uri saveBitmap(Bitmap bm, String dirPath) {
+        //新建文件夹用于存放裁剪后的图片
+        File tmpDir = new File(Environment.getExternalStorageDirectory() + "/" + dirPath);
+        if (!tmpDir.exists()){
+            tmpDir.mkdir();
+        }
+
+        //新建文件存储裁剪后的图片
+        File img = new File(tmpDir.getAbsolutePath() + "/avator.png");
+        try {
+            //打开文件输出流
+            FileOutputStream fos = new FileOutputStream(img);
+            //将bitmap压缩后写入输出流(参数依次为图片格式、图片质量和输出流)
+            bm.compress(Bitmap.CompressFormat.PNG, 85, fos);
+            //刷新输出流
+            fos.flush();
+            //关闭输出流
+            fos.close();
+            //返回File类型的Uri
+            return Uri.fromFile(img);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    /**
+     * 通过Uri传递图像信息以供裁剪
+     * @param uri
+     */
+    private void startImageZoom(Uri uri){
+        //构建隐式Intent来启动裁剪程序
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        //设置数据uri和类型为图片类型
+        intent.setDataAndType(uri, "image/*");
+        //显示View为可裁剪的
+        intent.putExtra("crop", true);
+        //裁剪的宽高的比例为1:1
+//        intent.putExtra("aspectX", 1);
+//        intent.putExtra("aspectY", 1);
+        //输出图片的宽高均为150
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        //裁剪之后的数据是通过Intent返回
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, CROP_CODE);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webview.canGoBack()) {
+            webview.goBack();// 返回前一个页面
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // 检测配置改动后执行相关操作
+    }
+}
